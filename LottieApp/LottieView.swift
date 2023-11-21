a7670ff2b88bfda581b60b81976f0686286a10f9
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-11-21 17:29:40 +0900

    yes

--- LottieApp/LottieView.swift
+++ LottieApp/LottieView.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+    var name: String
+    var animationView = LottieAnimationView()
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        let view = UIView(frame: .zero)
+        // 表示したいアニメーションのファイル名
+        animationView.animation = LottieAnimation.named(name)
+        // 比率
+        animationView.contentMode = .scaleAspectFit
+        // ループモード
+        animationView.loopMode = .loop
+        animationView.play()
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        NSLayoutConstraint.activate([
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+        return view
+    }
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<LottieView>) {
+    }
+}
